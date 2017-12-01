50d3426400f94c75e62bd9d9f88735a8b45b5601
masukomi <masukomi@masukomi.org>
2017-11-30 22:56:46 -0500

    added Table#formatted

--- spec/crystal_fmt/table_spec.cr
+++ spec/crystal_fmt/table_spec.cr
@@ -24,6 +24,62 @@ describe Table do
     t.columns.last.size.should(eq(2))
     t.columns.first.strings[0].should(eq("a"))
   end
+
+  it "should create one line of text per row" do 
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    t.formatted.split("\n").size.should(eq(2))
+  end
+  it "should format with | between each item" do
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    formatted = t.formatted
+    formatted.includes?("a | b").should(be_true())
+    formatted.includes?("c |  ").should(be_true())
+  end
+
+  it "should start rows with \"| \"" do
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    formatted = t.formatted
+    formatted.includes?("| a").should(be_true())
+    formatted.includes?("| c").should(be_true())
+  end
+  it "should end rows with \" |\"" do
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    formatted = t.formatted
+    formatted.includes?("b |").should(be_true())
+    formatted.includes?("  |").should(be_true())
+  end
+
+  it "should have a configurable left border" do
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    options = Hash(Symbol,String).new
+    options[:left_border] = "X "
+    formatted = t.formatted(t.columns,options) 
+    formatted.includes?("X a").should(be_true())
+    formatted.includes?("X c").should(be_true())
+  end
+  it "should have a configurable right border" do
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    options = Hash(Symbol,String).new
+    options[:right_border] = " X"
+    formatted = t.formatted(t.columns,options) 
+    formatted.includes?("b X").should(be_true())
+    formatted.includes?("  X").should(be_true())
+  end
+  it "should have a configurable divider" do
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    options = Hash(Symbol,String).new
+    options[:divider] = " X "
+    formatted = t.formatted(t.columns, options)
+    formatted.includes?("a X b").should(be_true())
+    formatted.includes?("c X  ").should(be_true())
+  end
 end
 
 
--- src/crystal_fmt/table.cr
+++ src/crystal_fmt/table.cr
@@ -1,6 +1,7 @@
 require "./missing_table_data.cr"
 class Table
   getter :columns
+  getter :data
   @columns : Array(Column)
   def initialize(@data : Array(Array(String | Nil)))
     column_count = @data[0].size
@@ -9,6 +10,36 @@ class Table
         "all rows must have an equal number of columns")
     end
     @columns = extract_columns(@data)
+  end
+
+  # TODO:  support options for: divider, left border, right border
+  def formatted(columns : Array(Column) = @columns, options : Hash(Symbol, String)  = Hash(Symbol,String).new) : String
+    divider = options.has_key?(:divider) ? options[:divider] : " | "
+    left_border = options.has_key?(:left_border) ? options[:left_border] : "| "
+    right_border = options.has_key?(:right_border) ? options[:right_border] : " |"
+    
+    column_max_idx = columns.size() -1
+    row_max_idx = columns.first.strings.size() -1
+    formatted_column_data = columns.map{|c| c.formatted } # Array(Array(String)
+    result = String.build do | str | 
+      (0..column_max_idx).each do |row_idx|
+        (0..row_max_idx).each do | col_idx |
+          if col_idx == 0
+            str << left_border
+          end
+          str << formatted_column_data[col_idx][row_idx]
+          if col_idx < column_max_idx 
+            str << divider
+          else
+            str << right_border
+            if row_idx < row_max_idx
+              str << "\n"
+            end
+          end
+         end
+      end
+    end
+    result
   end
   
   private def extract_columns(data : Array(Array(String | Nil))) : Array(Column)
