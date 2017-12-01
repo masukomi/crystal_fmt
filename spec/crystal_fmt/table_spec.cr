72a92b107e79e5a6c4c8b3f3a1150a59ae7a0f79
masukomi <masukomi@masukomi.org>
2017-11-30 23:40:55 -0500

    Addde Table#add_row(row) and Table#format(options)

--- spec/crystal_fmt/table_spec.cr
+++ spec/crystal_fmt/table_spec.cr
@@ -19,21 +19,64 @@ describe Table do
 
   it "should extract columns from data" do
     t = Table.new(default_data())
-    t.columns.size.should(eq(2))
-    t.columns.first.size.should(eq(2))
-    t.columns.last.size.should(eq(2))
-    t.columns.first.strings[0].should(eq("a"))
+    columns = t.extract_columns(t.data)
+    columns.size.should(eq(2))
+    columns.first.size.should(eq(2))
+    columns.last.size.should(eq(2))
+    columns.first.strings[0].should(eq("a"))
+    columns.last.strings[-1].should(eq(nil))
+  end
+  it "should extract columns from data after adding a row" do
+    t = Table.new(default_data())
+    t.add_row(Array(String|Nil).new+["e", "f"])
+    columns = t.extract_columns(t.data)
+    columns.size.should(eq(2))
+    columns.first.size.should(eq(3))
+    columns.last.size.should(eq(3))
+    columns.first.strings[0].should(eq("a"))
+    columns.last.strings[-1].should(eq("f"))
   end
 
   it "should create one line of text per row" do 
     t = Table.new(default_data())
     # [["a", "b"], ["c", nil]]
-    t.formatted.split("\n").size.should(eq(2))
+    t.format.split("\n").size.should(eq(2))
   end
+  it "should allow you to add rows" do 
+    t = Table.new(default_data())
+    t.add_row(Array(String|Nil).new+["e", "f"])
+    t.data.size.should(eq(3))
+    t.data.inspect.should(eq("[[\"a\", \"b\"], [\"c\", nil], [\"e\", \"f\"]]"))
+    # [["a", "b"], ["c", nil]]
+  end
+  it "should format correctly after adding a row" do
+    #because it didn't once
+    t = Table.new(default_data())
+    t.add_row(Array(String|Nil).new+["e", "f"])
+    t.data.size.should(eq(3))
+    t.data.inspect.should(eq("[[\"a\", \"b\"], [\"c\", nil], [\"e\", \"f\"]]"))
+    # [["a", "b"], ["c", nil]]
+    t.format.split("\n").size.should(eq(3))
+  end
+
+  it "should raise MissingTableData when added badly sized row" do 
+    t = Table.new(default_data())
+    # [["a", "b"], ["c", nil]]
+    expect_raises (MissingTableData) do
+      t.add_row(Array(String|Nil).new+["d"])
+    end
+  end
+  it "should allow you to add a row of any size to an empty table" do
+    t = Table.new()
+    t.add_row(Array(String|Nil).new+["d"])
+    t = Table.new()
+    t.add_row(Array(String|Nil).new+["e", "f"])
+  end
+
   it "should format with | between each item" do
     t = Table.new(default_data())
     # [["a", "b"], ["c", nil]]
-    formatted = t.formatted
+    formatted = t.format
     formatted.includes?("a | b").should(be_true())
     formatted.includes?("c |  ").should(be_true())
   end
@@ -41,14 +84,14 @@ describe Table do
   it "should start rows with \"| \"" do
     t = Table.new(default_data())
     # [["a", "b"], ["c", nil]]
-    formatted = t.formatted
+    formatted = t.format
     formatted.includes?("| a").should(be_true())
     formatted.includes?("| c").should(be_true())
   end
   it "should end rows with \" |\"" do
     t = Table.new(default_data())
     # [["a", "b"], ["c", nil]]
-    formatted = t.formatted
+    formatted = t.format
     formatted.includes?("b |").should(be_true())
     formatted.includes?("  |").should(be_true())
   end
@@ -58,7 +101,7 @@ describe Table do
     # [["a", "b"], ["c", nil]]
     options = Hash(Symbol,String).new
     options[:left_border] = "X "
-    formatted = t.formatted(t.columns,options) 
+    formatted = t.format(options) 
     formatted.includes?("X a").should(be_true())
     formatted.includes?("X c").should(be_true())
   end
@@ -67,7 +110,7 @@ describe Table do
     # [["a", "b"], ["c", nil]]
     options = Hash(Symbol,String).new
     options[:right_border] = " X"
-    formatted = t.formatted(t.columns,options) 
+    formatted = t.format(options) 
     formatted.includes?("b X").should(be_true())
     formatted.includes?("  X").should(be_true())
   end
@@ -76,7 +119,7 @@ describe Table do
     # [["a", "b"], ["c", nil]]
     options = Hash(Symbol,String).new
     options[:divider] = " X "
-    formatted = t.formatted(t.columns, options)
+    formatted = t.format(options)
     formatted.includes?("a X b").should(be_true())
     formatted.includes?("c X  ").should(be_true())
   end
--- src/crystal_fmt/table.cr
+++ src/crystal_fmt/table.cr
@@ -1,37 +1,50 @@
 require "./missing_table_data.cr"
 class Table
-  getter :columns
+  @data : Array(Array(String | Nil))
   getter :data
-  @columns : Array(Column)
-  def initialize(@data : Array(Array(String | Nil)))
-    column_count = @data[0].size
-    if ! @data.all?{|x|x.size == column_count}
+  setter :data
+  def initialize(@data : Array(Array(String | Nil)) = Array(Array(String | Nil)).new)
+    if (@data.size > 0)
+      column_count = @data[0].size
+      if ! @data.all?{|x|x.size == column_count}
+        raise MissingTableData.new(
+          "all rows must have an equal number of columns")
+      end
+    end
+  end
+
+  def add_row(row : Array(String | Nil))
+    if data.size > 0 && data[0].size != row.size
       raise MissingTableData.new(
         "all rows must have an equal number of columns")
     end
-    @columns = extract_columns(@data)
+    @data << row
   end
 
-  # TODO:  support options for: divider, left border, right border
-  def formatted(columns : Array(Column) = @columns, options : Hash(Symbol, String)  = Hash(Symbol,String).new) : String
-    divider = options.has_key?(:divider) ? options[:divider] : " | "
-    left_border = options.has_key?(:left_border) ? options[:left_border] : "| "
-    right_border = options.has_key?(:right_border) ? options[:right_border] : " |"
-    
+  def format(options : Hash(Symbol, String) = Hash(Symbol,String).new) : String
+
+    options[:divider] = " | " unless options.has_key?(:divider)
+    options[:left_border] = "| " unless options.has_key?(:left_border)
+    options[:right_border]  = " |" unless options.has_key?(:right_border)
+    formatted(extract_columns(@data), options)
+  end
+  
+  private def formatted(columns : Array(Column), options : Hash(Symbol, String)) : String
     column_max_idx = columns.size() -1
     row_max_idx = columns.first.strings.size() -1
     formatted_column_data = columns.map{|c| c.formatted } # Array(Array(String)
+    
     result = String.build do | str | 
-      (0..column_max_idx).each do |row_idx|
-        (0..row_max_idx).each do | col_idx |
+      (0..row_max_idx).each do |row_idx|
+        (0..column_max_idx).each do | col_idx |
           if col_idx == 0
-            str << left_border
+            str << options[:left_border]
           end
           str << formatted_column_data[col_idx][row_idx]
           if col_idx < column_max_idx 
-            str << divider
+            str << options[:divider]
           else
-            str << right_border
+            str << options[:right_border]
             if row_idx < row_max_idx
               str << "\n"
             end
@@ -42,7 +55,7 @@ class Table
     result
   end
   
-  private def extract_columns(data : Array(Array(String | Nil))) : Array(Column)
+  def extract_columns(data : Array(Array(String | Nil))) : Array(Column)
     column_count = data[0].size
     row_count = data.size
     result = Array(Column).new(column_count)
